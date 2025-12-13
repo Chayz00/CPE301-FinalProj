cf00d0feb140944497036ebc218aff8a612b104b
Chayz00 <chayton_zuniga@mail.tmcc.edu>
2025-12-12 20:19:30 -0800

    Update FinalProject.ino
    Version 4) DHT sensor- Temp and Humidity( with temp transition logic)

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <DHT.h>   
 
 //System state
 typedef enum {
@@ -11,22 +12,23 @@ typedef enum {
 
 volatile SystemState systemState = STATE_DISABLED;
 volatile uint8_t    startPending = 0;   
+
 // LED pins (22–25 = PA0–PA3)
 #define LED_DDR DDRA
 #define LED_PORT PORTA
 
 // Yellow= disabled, Green = idle, Red= error,Blue = running
 #define LED_DISABLED_BIT PA0    // pin 22= Yellow
-#define LED_IDLE_BIT  PA1       // pin 23 = Green
-#define LED_ERROR_BIT PA2      // pin 24 = Red
-#define LED_RUNNING_BIT PA3   // pin 25 = Blue
+#define LED_IDLE_BIT     PA1    // pin 23 = Green
+#define LED_ERROR_BIT    PA2    // pin 24 = Red
+#define LED_RUNNING_BIT  PA3    // pin 25 = Blue
 
 //Buttons 
-#define BUTTON_DDR  DDRC
-#define BUTTON_PORT  PORTC
-#define BUTTON_PIN_REG  PINC
+#define BUTTON_DDR       DDRC
+#define BUTTON_PORT      PORTC
+#define BUTTON_PIN_REG   PINC
 #define BUTTON_STOP_BIT  PC7      // pin 30
-#define BUTTON_RESET_BIT PC6     // pin 31
+#define BUTTON_RESET_BIT PC6      // pin 31
 
 // Start PORTE (INT4 - pin 2)
 #define BUTTON_START_BIT PE4     // pin 2
@@ -34,23 +36,41 @@ volatile uint8_t    startPending = 0;
 // Water sensor (ADC channel 0)
 static const unsigned char WATER_CHANNEL = 0;    // A0
 static unsigned int  WATER_THRESHOLD = 200;  
-volatile unsigned char *adcMux = (unsigned char*) 0x7C;
+
+volatile unsigned char *adcMux   = (unsigned char*) 0x7C;
 volatile unsigned char *adcCtrlB = (unsigned char*) 0x7B;
 volatile unsigned char *adcCtrlA = (unsigned char*) 0x7A;
-volatile unsigned int  *adcData = (unsigned int  *) 0x78;
+volatile unsigned int  *adcData  = (unsigned int  *) 0x78;
+
 void initAdc(void);
 unsigned int readAdc(unsigned char channel);
 uint8_t isWaterLow(void);
+
+//  DHT11 (temp/humidity)
+#define DHT_PIN   7
+#define DHT_TYPE  DHT11
+DHT dht(DHT_PIN, DHT_TYPE);
+
+int     currentTempC    = 0;
+int     currentHumidity = 0;
+uint8_t hasValidDht     = 0;
+
+void updateDhtIfNeeded(void);
+
+// Temp transition tresholds
+const uint8_t TEMP_ON_THRESHOLD  = 26;  // go to RUNNING when >= 26
+const uint8_t TEMP_OFF_THRESHOLD = 24;  // go back to IDLE when <= 24
 
 //UART0
 #define RDA 0x80
 #define TBE 0x20
 
-volatile unsigned char *uartStatusA = (unsigned char *)0x00C0;
-volatile unsigned char *uartControlB= (unsigned char *)0x00C1;
-volatile unsigned char *uartControlC= (unsigned char *)0x00C2;
-volatile unsigned int  *uartBaud = (unsigned int  *)0x00C4;
-volatile unsigned char *uartData = (unsigned char *)0x00C6;
+volatile unsigned char *uartStatusA  = (unsigned char *)0x00C0;
+volatile unsigned char *uartControlB = (unsigned char *)0x00C1;
+volatile unsigned char *uartControlC = (unsigned char *)0x00C2;
+volatile unsigned int  *uartBaud     = (unsigned int  *)0x00C4;
+volatile unsigned char *uartData     = (unsigned char *)0x00C6;
+
 void uartInit(int baud);
 unsigned char uartKbHit(void);
 unsigned char uartGetChar(void);
@@ -80,7 +100,10 @@ void setup()
   setupPins();
   initAdc();
   uartInit(9600);
+  dht.begin();  
+
   attachInterrupt(digitalPinToInterrupt(2), onStartButton, FALLING);
+
   systemState = STATE_DISABLED;
   updateLeds();
   uartPrintStr("System booted. State=DISABLED");
@@ -91,16 +114,27 @@ void setup()
 void loop()
 {
   handleButtonsSensorsAndState();
-  logStatusIfNeeded();   
+
+  // No monitoring/logging in DISABLED
+  if (systemState != STATE_DISABLED) {
+    updateDhtIfNeeded();
+    logStatusIfNeeded();   
+  }
 }
 
 // GPIO
 void setupPins(void)
 {
   // LED 
-  LED_DDR |= (1 << LED_DISABLED_BIT) | (1 << LED_IDLE_BIT)     | (1 << LED_ERROR_BIT)  | (1 << LED_RUNNING_BIT);
+  LED_DDR |= (1 << LED_DISABLED_BIT) |
+             (1 << LED_IDLE_BIT)     |
+             (1 << LED_ERROR_BIT)    |
+             (1 << LED_RUNNING_BIT);
 
-  LED_PORT &= ~((1 << LED_DISABLED_BIT) | (1 << LED_IDLE_BIT) | (1 << LED_ERROR_BIT) | (1 << LED_RUNNING_BIT));
+  LED_PORT &= ~((1 << LED_DISABLED_BIT) |
+                (1 << LED_IDLE_BIT)     |
+                (1 << LED_ERROR_BIT)    |
+                (1 << LED_RUNNING_BIT));
 
   // Stop & Reset button
   BUTTON_DDR  &= ~((1 << BUTTON_STOP_BIT) | (1 << BUTTON_RESET_BIT)); // inputs
@@ -109,13 +143,15 @@ void setupPins(void)
   // Start button 
   DDRE  &= ~(1 << BUTTON_START_BIT);
   PORTE |=  (1 << BUTTON_START_BIT);
+
+  /
 }
 
 //LED update 
 void updateLeds(void)
 {
   // All off
-  LED_PORT &= ~((1 << LED_DISABLED_BIT) | (1 << LED_IDLE_BIT)  | (1 << LED_ERROR_BIT)  | (1 << LED_RUNNING_BIT));
+  LED_PORT &= ~((1 << LED_DISABLED_BIT) | (1 << LED_IDLE_BIT)  | (1 << LED_ERROR_BIT) | (1 << LED_RUNNING_BIT));
 
   switch (systemState) {
     case STATE_DISABLED:
@@ -159,7 +195,6 @@ void handleButtonsSensorsAndState(void)
     if (systemState == STATE_DISABLED) {
       setState(STATE_IDLE);
     }
-    
   }
 
   // Stop
@@ -175,15 +210,27 @@ void handleButtonsSensorsAndState(void)
     }
   }
 
-  // IDLE low water to ERROR
+  // IDLE/RUNNING (ERROR when water is low)
   if (systemState == STATE_IDLE || systemState == STATE_RUNNING) {
     if (isWaterLow()) {
       setState(STATE_ERROR);
+      return;   
     }
   }
-
- 
+ //Check for valid 
+  if (!hasValidDht) {
+    return;
+  }
+ // Check water level 
+  if (!isWaterLow()) {
+    if (systemState == STATE_IDLE && currentTempC >= TEMP_ON_THRESHOLD) {
+      setState(STATE_RUNNING);
+    } else if (systemState == STATE_RUNNING && currentTempC <= TEMP_OFF_THRESHOLD) {
+      setState(STATE_IDLE);
+    }
+  }
 }
+
 //ADC - Lab 8 
 void initAdc(void) 
 {
@@ -222,6 +269,34 @@ uint8_t isWaterLow(void)
   }
 }
 
+//  DHT11 helper 
+void updateDhtIfNeeded(void)
+{
+  // No monitoring of temp in DISABLED
+  if (systemState == STATE_DISABLED) {
+    return;
+  }
+
+  static unsigned long lastReadMs = 0;
+  unsigned long now = millis();
+
+  if (now - lastReadMs < 2000) {
+    return;
+  }
+  lastReadMs = now;
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // Celsius
+
+  if (isnan(h) || isnan(t)) {
+    hasValidDht = 0;
+    return;
+  }
+
+  currentHumidity = (int)h;
+  currentTempC    = (int)t;
+  hasValidDht     = 1;
+}
 
 //  UART helpers
 void uartInit(int baud)
@@ -229,7 +304,7 @@ void uartInit(int baud)
   unsigned long FCPU = 16000000UL;
   unsigned int tbaud = (unsigned int)(FCPU / 16UL / baud - 1UL);
 
-  *uartStatusA = 0x20;   
+  *uartStatusA  = 0x20;   
   *uartControlB = 0x18;  
   *uartControlC = 0x06;  
   *uartBaud     = tbaud;
@@ -308,6 +383,11 @@ void logStateChange(SystemState oldState, SystemState newState)
 
 void logStatusIfNeeded(void)
 {
+ // No monitoring/logging in DISABLED
+  if (systemState == STATE_DISABLED) {
+    return;
+  }
+
   static unsigned long lastLogMs = 0;
   unsigned long now = millis();
 
@@ -317,7 +397,19 @@ void logStatusIfNeeded(void)
 
     uartPrintStr("Status: ");
     uartPrintStr(stateToString(systemState));
-    uartPrintStr(" | waterADC=");
+    uartPrintStr(" | ");
+
+    if (hasValidDht) {
+      uartPrintStr("T=");
+      uartPrintUint((unsigned int)currentTempC);
+      uartPrintStr("C H=");
+      uartPrintUint((unsigned int)currentHumidity);
+      uartPrintStr("% | ");
+    } else {
+      uartPrintStr("T=-- H=-- | ");
+    }
+
+    uartPrintStr("waterADC=");
     uartPrintUint(reading);
     uartPrintStr(" | low=");
     uartPutChar(isWaterLow() ? '1' : '0');
